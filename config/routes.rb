244d88d8d61328b27f58d3bfdf1089e7bdd42947
andynhi <nhi.atn@gmail.com>
2017-02-14 12:04:22 -0800

    integrate argis mapservers from rfp data sources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root "hello_world#index"
 
   get "/hello_world" => "hello_world#index"
+  get "/maps" => "maps#index"
 
   namespace :admin do
     resources :campaign, only: [:index]
   end
+
 end
