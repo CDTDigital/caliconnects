f6c8b4eab79a312d9d03e7bd30d1f7742485ef7f
Alex Emrie and Savannah Worth <aemrie+worth@us.ibm.com>
2017-02-20 15:10:08 -0800

    adds all fields to user sign up

--- spec/features/hello_world_spec.rb
+++
@@ -1,7 +0,0 @@
-describe "greeting page" do
-  it "says hello world" do
-    visit "/"
-
-    expect(page).to have_content("hello world!")
-  end
-end
---
+++ spec/features/sign_up_feature_spec.rb
@@ -0,0 +1,22 @@
+describe "user sign up" do
+  it "lets users create an account" do
+    visit new_user_registration_path
+
+    fill_in :user_first_name, with: "brenda"
+    fill_in :user_last_name, with: "awesome"
+
+    # click_button "Next"
+
+    fill_in :user_email, with: "brenda@example.com"
+
+    # click_button "Next"
+
+    fill_in :user_password, with: "jillsecret"
+
+    # click_button "Next"
+
+    fill_in :user_phone, with: "4155555555"
+
+    # click_button "Next"
+  end
+end
