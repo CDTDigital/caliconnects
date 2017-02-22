b233d1b9db34a20c8add6e266baab5c695378542
Andy Nguyen and Savannah Worth <atnguyen+worth@us.ibm.com>
2017-02-22 10:59:09 -0800

    route sign up, login and home links

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -6,5 +6,21 @@ describe "welcome" do
       expect(page).to have_content "Sign up for emergency alerts!"
       expect(page).to have_content "Our new system can now send location based alerts. This means if you get an alert, the system has intelligently determined that you NEED the details to stay safe, because you are within the affected area. This also means you won't receive alerts that don't affect you."
     end
+
+    it "links to the sign up page" do
+      visit root_path
+
+      click_button "Sign Up"
+
+      expect(current_path).to eq new_user_registration_path
+    end
+
+    it "links to the login page" do
+      visit root_path
+
+      click_button "Login"
+
+      expect(current_path).to eq new_user_session_path
+    end
   end
 end
