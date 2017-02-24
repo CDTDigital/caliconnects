750002698c15fe7b5994cbe190ac5aa0a24e2193
Alex Emrie and Savannah Worth <aemrie+worth@us.ibm.com>
2017-02-23 16:49:32 -0800

    adds expected browser behavior to signup flow

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,17 +1,42 @@
 $(document).ready(function(){
+    if($("#signup-marker")) {
+        window.location = "#1";
 
-    $(".signup-page__next-button").on("click", function(){
-        var visible = $(".signup-page:visible").attr("data-pagenumber");
-        var currPageNumber = Number(visible);
-        var nextPageNumber = currPageNumber + 1;
+        var moveToPreviousPage = function() {
+            var hash = window.location.hash;
+            var hashNumber = hash.split("#")[1];
 
-        var currPageSelector = "#signup-page-" + currPageNumber;
-        var nextPageSelector = "#signup-page-" + nextPageNumber;
+            var currPageSelector = "#signup-page-" + (hashNumber);
 
-        $(currPageSelector).hide();
-        $(nextPageSelector).show();
-    });
+            $(".signup-page").hide();
+            $(currPageSelector).show();
+        };
 
-    // email, password, password matching, phone, location
+        var moveToNextPage = function(){
+            var visible = $(".signup-page:visible").attr("data-pagenumber");
+            var currPageNumber = Number(visible);
+            var nextPageNumber = currPageNumber + 1;
 
+            var currPageSelector = "#signup-page-" + currPageNumber;
+            var nextPageSelector = "#signup-page-" + nextPageNumber;
+
+            window.location = "#" + nextPageNumber;
+
+            $(currPageSelector).hide();
+            $(nextPageSelector).show();
+        };
+
+        var clickTheNextButton = function (e) {
+            if (e.which == 13) {
+                $("#signup-page__next-button").trigger("click");
+                return false;
+            }
+        };
+
+        $(".signup-page__next-button").on("click", moveToNextPage);
+
+        $(window).bind('hashchange', moveToPreviousPage);
+
+        $("input").keypress(clickTheNextButton);
+    }
 });
