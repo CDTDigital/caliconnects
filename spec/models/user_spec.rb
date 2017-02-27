43e9f6d78e76186d833349eb0123af97e8d7b5b4
Alex Emrie and Andy Nguyen <aemrie+atnguyen@us.ibm.com>
2017-02-27 07:43:27 -0800

    apply shoulda matchers for model validation testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,6 @@
 describe User do
+  describe 'user information' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,16 @@ include ApplicationHelper
 
 # require 'shoulda/matchers'
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    with.library :rails
+  end
+end
+
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
