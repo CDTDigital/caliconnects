e3f2af4e2ebcbe19cf18c411ba24035d91f82cdf
Savannah Worth <worth@us.ibm.com>
2017-03-03 09:42:49 -0800

    updates sign up page copy

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -3,8 +3,7 @@ describe "welcome" do
     it "shows the landing page" do
       visit root_path
       expect(page).to have_content "We want you to stay safe!"
-      expect(page).to have_content "Sign up for emergency alerts!"
-      expect(page).to have_content "Our new system can now send location based alerts. This means if you get an alert, the system has intelligently determined that you NEED the details to stay safe, because you are within the affected area. This also means you won't receive alerts that don't affect you."
+      expect(page).to have_content "Sign up to receive alerts from your state and local government"
     end
 
     it "links to the sign up page" do
